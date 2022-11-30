76b9eb796e51382fef9358e9ebb34b8a37c40d6f
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-11-30 22:30:12 +0900

    etc. 공용 팔레트, 아이콘, 심볼 다운로드(assets)

---
+++ src/constants/palette.ts
@@ -0,0 +1,19 @@
+export const palette = {
+  white: '#fff',
+  black: '#000',
+  blue: '1b86ff',
+  green: '#07be75',
+  orange: '#ff8934',
+  red: '#ff4e79',
+  burgundy: 'rgb(76, 23, 36)',
+  grey: {
+    100: '#fafafa',
+    200: '#f2f2f2',
+    300: '#ddd',
+    400: '#aaa',
+    500: '#888',
+    600: '#333',
+    700: '#1c1c1c',
+    800: '#111',
+  },
+};
