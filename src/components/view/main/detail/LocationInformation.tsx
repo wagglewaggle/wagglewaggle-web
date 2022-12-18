d83b45c7a4520f670d68713da4cdfc8f34482647
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-12-18 17:31:01 +0900

    feature. 카카오 지도 관련 데이터 추가 및 기능 구현(임시 데이터 적용)

--- src/components/view/main/detail/LocationInformation.tsx
+++ src/components/view/main/detail/LocationInformation.tsx
@@ -1,7 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { IconButton } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { palette } from 'constants/palette';
+import { geometry } from 'constants/geometry';
+import { dataSample } from 'constants/dataSample';
+import { useStore } from 'stores';
+import { locationDataType } from 'types/typeBundle';
 import navigationIcon from 'assets/icons/navigation-icon.svg';
 
 const useStyles = makeStyles(() => ({
@@ -67,33 +71,98 @@ declare global {
 }
 
 const LocationInformation = () => {
+  const [locationData, setLocationData] = useState<locationDataType | null>(null);
+  const [locationAddress, setLocationAddress] = useState<string | null>(null);
   const mapRef = useRef<HTMLDivElement>(null);
   const classes = useStyles();
+  const { CustomDialogStore } = useStore().MobxStore;
+
+  const processGeometryCoordinates = (
+    coordinates: [number, number][][],
+    polygonPath: [number, number][]
+  ) => {
+    coordinates.forEach((coordinateArr: [number, number][]) => {
+      coordinateArr.forEach(([thisLatitude, thisLongitude]: [number, number]) => {
+        polygonPath.push(new window.kakao.maps.LatLng(thisLatitude, thisLongitude));
+      });
+    });
+  };
+
+  const highlightMap = useCallback(() => {
+    if (!locationData) return;
+    const locationName = locationData.name;
+    const coordinates: [number, number][][] | [number, number][][][] =
+      geometry[locationName].coordinates;
+    const geometryType: 'Polygon' | 'MultiPolygon' = geometry[locationName].type;
+    const polygonPath: [number, number][] = [];
+    if (geometryType === 'Polygon') {
+      processGeometryCoordinates(coordinates as [number, number][][], polygonPath);
+    } else {
+      (coordinates as [number, number][][][]).forEach((subCoordinates: [number, number][][]) =>
+        processGeometryCoordinates(subCoordinates, polygonPath)
+      );
+    }
+    const polygon = new window.kakao.maps.Polygon({
+      path: polygonPath,
+      strokeWeight: 1,
+      strokeColor: palette.orange,
+      strokeOpacity: 1,
+      strokeStyle: 'solid',
+      fillColor: palette.orange,
+      fillOpacity: 0.4,
+    });
+    return polygon;
+  }, [locationData]);
+
+  const getAddress = (lng: number, lat: number) => {
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    geocoder.coord2Address(
+      lng,
+      lat,
+      (result: { address: { address_name: string } }[], status: 'OK' | 'ZERO_RESULT') => {
+        setLocationAddress(status === 'OK' ? result[0]?.address?.address_name : null);
+      }
+    );
+  };
+
+  const getKakaoMap = useCallback(() => {
+    if (!locationData) return;
+    const center: number[] = [locationData.x, locationData.y];
+    const [latitude, longitude] = center;
+    window.kakao.maps.load(() => {
+      const map = new window.kakao.maps.Map(mapRef.current, {
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+        level: 7,
+      });
+      const marker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(latitude, longitude),
+      });
+      marker.setMap(map);
+      highlightMap().setMap(map);
+      const centerCoords = map.getCenter();
+      getAddress(centerCoords.getLng(), centerCoords.getLat());
+    });
+  }, [highlightMap, locationData]);
+
+  const setAccidentLists = useCallback(() => {
+    if (locationData && locationData.accidents.length > 0) {
+      CustomDialogStore.openAccidentDialog(locationData.accidents);
+    }
+  }, [CustomDialogStore, locationData]);
 
   useEffect(() => {
+    setAccidentLists();
     const mapScript = document.createElement('script');
     mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_APP_KEY}&autoload=false`;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_APP_KEY}&autoload=false&libraries=services`;
     document.head.appendChild(mapScript);
-
-    const getKakaoMap = () => {
-      // 임시 dummy 좌표
-      const latitude: number = 33.450701;
-      const longitude: number = 126.570667;
-      window.kakao.maps.load(() => {
-        const map = new window.kakao.maps.Map(mapRef.current, {
-          center: new window.kakao.maps.LatLng(latitude, longitude),
-          level: 3,
-        });
-        const marker = new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(latitude, longitude),
-        });
-        marker.setMap(map);
-      });
-    };
 
     mapScript.addEventListener('load', getKakaoMap);
     return () => mapScript.removeEventListener('load', getKakaoMap);
+  }, [locationData, setAccidentLists, getKakaoMap]);
+
+  useEffect(() => {
+    setLocationData(dataSample);
   }, []);
 
   return (
@@ -103,8 +172,8 @@ const LocationInformation = () => {
         <div className={classes.map} ref={mapRef} />
         <div className={classes.description}>
           <div className={classes.textArea}>
-            <div className={classes.name}>카카오 스페이스닷원</div>
-            <div className={classes.address}>제주특별자치도 제주시 첨단로 242</div>
+            <div className={classes.name}>{locationData?.name}</div>
+            <div className={classes.address}>{locationAddress || ''}</div>
           </div>
           <IconButton sx={{ border: `1px solid ${palette.white}`, padding: '3px' }}>
             <img className={classes.navigationIcon} src={navigationIcon} alt='navigation' />
