542fcf478336d05eefb8fc6370a609c5d8290aa3
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2022-12-29 01:02:25 +0900

    feature. MobX ThemeStore 추가

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,8 +1,9 @@
-import { ScreenSizeStore, CustomDialogStore, ErrorStore, LocationStore } from 'stores';
+import { ScreenSizeStore, CustomDialogStore, ErrorStore, LocationStore, ThemeStore } from 'stores';
 
 export default class RootStore {
   ScreenSizeStore: ScreenSizeStore = new ScreenSizeStore();
   CustomDialogStore: CustomDialogStore = new CustomDialogStore();
   ErrorStore: ErrorStore = new ErrorStore();
   LocationStore: LocationStore = new LocationStore();
+  ThemeStore: ThemeStore = new ThemeStore();
 }
