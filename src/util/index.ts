dcba0a5d0689fc962df2de26c727c3eeeb96323d
unknown <kskim625@gmail.com>
2023-03-26 19:48:29 +0900

    fix. 지도 선택된 마커, overlay coloring

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,3 +2,4 @@ export { default as getTimeDiff } from './getTimeDiff';
 export { default as getImageSymbol } from './getImageSymbol';
 export { default as initPlaceData } from './initPlaceData';
 export { default as handleShareLinkClick } from './handleShareLinkClick';
+export { default as getMapSelectedSymbol } from './getMapSelectedSymbol'
