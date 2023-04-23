3917caeff389d0e7282181de7ed22e6b986ae8d2
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-04-23 18:29:26 +0900

    fix. 랜딩 페이지 업데이트

--- src/components/view/browserPage/Content.tsx
+++ src/components/view/browserPage/Content.tsx
@@ -1,32 +1,23 @@
 import { styled } from '@mui/material';
-import { observer } from 'mobx-react';
-import { useStore } from 'stores';
 import { palette } from 'constants/';
 import { ContentPropsType } from '.';
 
 const Content = (props: ContentPropsType) => {
-  const { idx, headerLogo, title, content, contentImage, subContentImage, shadyBackground } = props;
-  const { ScreenSizeStore } = useStore().MobxStore;
-  const { screenWidth } = ScreenSizeStore;
-  const device = screenWidth < 768 ? 'mobile' : screenWidth < 1024 ? 'tablet' : 'pc';
+  const { idx, headerLogo, title, content, contentImage, shadyBackground } = props;
 
   return (
-    <Wrap shady={shadyBackground} device={device} paddingBottom={idx === 1 ? 24 : 44}>
-      <SubWrap device={device}>
+    <Wrap shady={shadyBackground}>
+      <SubWrap>
         <ContentWrap>
           <HeaderLogo src={headerLogo} alt='header' />
-          <TextWrap>
+          <ContentWrap>
             <TextTitle>{title}</TextTitle>
             <TextContent>{content}</TextContent>
-          </TextWrap>
-          {device !== 'mobile' && subContentImage && (
-            <SubContentImage src={subContentImage} alt='sub-content' device={device} />
-          )}
+          </ContentWrap>
         </ContentWrap>
         <ContentImage
           src={contentImage}
           alt='content'
-          device={device}
           height={idx === 0 ? 427 : idx === 1 ? 437 : 478}
         />
       </SubWrap>
@@ -34,45 +25,36 @@ const Content = (props: ContentPropsType) => {
   );
 };
 
-export default observer(Content);
+export default Content;
 
 const Wrap = styled('div', {
-  shouldForwardProp: (prop: string) => !['shady', 'device', 'paddingBottom'].includes(prop),
-})<{ shady: boolean; device: 'mobile' | 'tablet' | 'pc'; paddingBottom: number }>(
-  ({ shady, device, paddingBottom }) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    padding: `56px ${device === 'mobile' ? 24 : 56}px ${paddingBottom}px`,
-    width: `calc(100% - ${device === 'mobile' ? 48 : 112}px)`,
-    backgroundColor: shady ? palette.grey[100] : palette.white,
-    zIndex: 0,
-  })
-);
+  shouldForwardProp: (prop: string) => prop !== 'shady',
+})<{ shady: boolean }>(({ shady }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '56px 0px 44px',
+  width: '100%',
+  backgroundColor: shady ? palette.grey[100] : palette.white,
+  zIndex: 0,
+}));
 
 const HeaderLogo = styled('img')({
   width: 48,
   height: 48,
 });
 
-const SubWrap = styled('div', {
-  shouldForwardProp: (prop: string) => prop !== 'device',
-})<{ device: 'mobile' | 'tablet' | 'pc' }>(({ device }) => ({
+const SubWrap = styled('div')({
   display: 'flex',
-  flexDirection: device === 'mobile' ? 'column' : 'row',
+  flexDirection: 'column',
   justifyContent: 'space-between',
-  alignItems: device === 'mobile' ? 'center' : 'flex-start',
+  alignItems: 'center',
   width: '100%',
-  maxWidth: 656,
-}));
+});
 
 const ContentWrap = styled('div')({
   display: 'flex',
   flexDirection: 'column',
-});
-
-const TextWrap = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
+  alignItems: 'center',
 });
 
 const TextTitle = styled('div')({
@@ -82,6 +64,7 @@ const TextTitle = styled('div')({
   fontWeight: 600,
   lineHeight: '24px',
   whiteSpace: 'pre-line',
+  textAlign: 'center',
 });
 
 const TextContent = styled('div')({
@@ -90,20 +73,16 @@ const TextContent = styled('div')({
   fontWeight: 400,
   lineHeight: '20px',
   whiteSpace: 'pre-line',
+  textAlign: 'center',
 });
 
 const ContentImage = styled('img', {
-  shouldForwardProp: (prop: string) => !['device', 'height'].includes(prop),
-})<{ device: 'mobile' | 'tablet' | 'pc'; height: number }>(({ device, height }) => ({
-  marginTop: device === 'mobile' ? 32 : 64,
+  shouldForwardProp: (prop: string) => prop !== 'height',
+})<{ height: number }>(({ height }) => ({
+  marginTop: 32,
   width: 327,
   height,
-}));
-
-const SubContentImage = styled('img', {
-  shouldForwardProp: (prop: string) => prop !== 'device',
-})<{ device: 'mobile' | 'tablet' | 'pc' }>(({ device }) => ({
-  marginTop: device === 'mobile' ? 0 : 243,
-  width: 96,
-  height: 111,
+  transform: 'translateZ(0)',
+  imageRendering: '-webkit-optimize-contrast',
+  backfaceVisibility: 'hidden',
 }));
