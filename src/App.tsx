664a2fafbe0ca56cdf600ab6f29755cecca7b414
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-04-23 12:50:17 +0900

    hotfix. 랜딩 페이지 접속 안되던 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ const App = observer(() => {
                 element={
                   isWebView ? (
                     <PrivateRoutes />
-                  ) : isMobile ? (
+                  ) : window.location.pathname !== '/' && isMobile ? (
                     <DeepLinkRoutes />
                   ) : (
                     <Navigate replace to='/landing' />
