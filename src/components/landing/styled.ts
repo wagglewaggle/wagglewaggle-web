d8e52d0c510888aa45230e5f02a95fdec14c86bd
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-02 00:35:49 +0900

    fix. 랜딩 페이지 컨텐츠가 정중앙에 오도록 수정, 로티 screenType에 따라 위치 변경

--- src/components/landing/styled.ts
+++ src/components/landing/styled.ts
@@ -24,7 +24,9 @@ export const BackgroundImage = styled('div', {
   zIndex: 1,
 }));
 
-export const Lottie = styled('div')({
+export const Lottie = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'screenType',
+})<{ screenType: ScreenType }>(({ screenType }) => ({
   position: 'absolute',
   top: 0,
   left: 0,
@@ -32,13 +34,23 @@ export const Lottie = styled('div')({
   height: '100vh',
   zIndex: 2,
   overflow: 'hidden',
-});
+  transform: `translateY(${screenType === 'pc' ? 0 : -11.5}rem)`,
+}));
 
 export const Logo = styled(Icon)({
-  position: 'relative',
+  position: 'absolute',
   top: '1rem',
   left: '2.5rem',
   zIndex: 2,
+});
+
+export const Content = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
 });
 
 export const Title = styled('div', {
@@ -46,7 +58,6 @@ export const Title = styled('div', {
 })<{ screenType: ScreenType }>(({ screenType }) => ({
   display: 'flex',
   justifyContent: 'center',
-  marginTop: `${screenType === 'pc' ? 17.438 : screenType === 'tablet' ? 19.5 : 7.188}rem`,
   marginBottom: `${screenType === 'pc' ? 2.5 : 2}rem`,
   fontSize: `${screenType !== 'pc' ? 2.5 : 4}rem`,
   fontWeight: 600,
@@ -76,7 +87,7 @@ export const CustomButton = styled(Button, {
 })<{ screenType: ScreenType }>(({ screenType }) => ({
   display: 'flex',
   padding: '1.125rem 1.5rem',
-  margin: `${screenType !== 'pc' ? 5.5 : 4}rem auto 0 auto`,
+  margin: `${screenType !== 'pc' ? 3 : 4}rem auto 0 auto`,
   width: '12.75rem',
   color: palette.white,
   fontSize: '1.125rem',
