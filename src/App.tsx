c85c74c63128519e0865c4eaddd256330231a9ad
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-06 21:28:23 +0900

    etc. react-helmet 라이브러리 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useLayoutEffect, useEffect, useMemo } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import { observer } from 'mobx-react';
 import useResizeObserver from 'use-resize-observer';
 import { GlobalStyles, styled } from '@mui/material';
@@ -70,9 +69,6 @@ const App = observer(() => {
 
   return (
     <Wrap isDarkTheme={isDarkTheme}>
-      <Helmet>
-        <meta http-equiv='Content-Security-Policy' content='media-src * blob:' />
-      </Helmet>
       <GlobalStyles styles={scrollbarDesign} />
       <CreateStore.Provider value={{ MobxStore }}>
         <ServiceWrap ref={ref} status={sessionStorage.getItem(projectStatusSessionStorageKey)}>
