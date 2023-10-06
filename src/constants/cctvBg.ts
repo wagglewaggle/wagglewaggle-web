036c37eaca3b62297979f6904eec35423e3c616d
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-06 22:13:10 +0900

    fix. KBS cctv bg 장소명 오류 수정

--- src/constants/cctvBg.ts
+++ src/constants/cctvBg.ts
@@ -6,10 +6,10 @@ import seoulgwangjang from 'assets/cctvBg/중구 서울광장.png';
 import yangjaechun from 'assets/cctvBg/서초 양재동(양재천).png';
 
 export const cctvBg: Record<string, Record<string, string>> = {
-  '월드컵 공원': { 홍제천: hongjaechun },
+  월드컵공원: { 홍제천: hongjaechun },
   강남역: { '강남 강남대로': gangnamdaero, '서초 서초대로': seochodaero },
-  여의도역: { '영등포 여의도': yeouido },
-  '여의도 한강공원': { '영등포 여의도': yeouido },
+  여의도: { '영등포 여의도': yeouido },
+  여의도한강공원: { '영등포 여의도': yeouido },
   서울광장: { '중구 서울광장': seoulgwangjang },
   양재역: { '서초 양재동(양재천)': yangjaechun },
 };
